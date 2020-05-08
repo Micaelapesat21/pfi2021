308217e37e813d911757449d596e0a3e6c03947f
Esteban Gueicha <estebnauquito181@gmail.com>
2020-05-07 22:14:06 -0300

    nuevo

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 import {Switch,Route} from 'react-router-dom';
 import Inicio from './Paginas/Inicio'
-import Perfil from './Paginas/Perfil'
+
 
 function App() {
   return (
     <Switch>
       <Route exact path='/' component={Inicio} />
-      <Route path='perfil' component={Perfil}/>
+    
     </Switch>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
+
 import {MuiThemeProvider, createMuiTheme} from '@material-ui/core'
 
 
@@ -43,4 +43,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
