9c83ea8ffcf2f2cced0a2f576a258020199cb490
edaluzrappi <emiliano.daluz@rappi.com>
2020-11-10 23:53:18 -0300

    lottie actualizado

--- src/Paginas/LoadingPage/Index.js
+++ src/Paginas/LoadingPage/Index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Lottie from 'react-lottie';
-import animationData from '../../AnimationJson/worldLoading.json'
+import animationData from '../../AnimationJson/30304-back-to-school.json'
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid } from '@material-ui/core';
 
 
 const useStyles = makeStyles(theme => ({
    root: {
-      backgroundColor: "#1565c0",
+      backgroundColor: "#2c699a",
       backgroundPosition: "center center",
       backgroundRepeat: "no-repeat",
       backgroundAttachment: "fixed",
